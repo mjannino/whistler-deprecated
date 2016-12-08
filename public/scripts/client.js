53a0a57cda35940fa00e782ec822dccd4da48806
Mike <michaeljannino@gmail.com>
2016-12-08 00:03:08 -0500

    Potential fix

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,55 +1,72 @@
+import _ from 'lodash';
+import without from 'lodash-without';
+import CryptoSession from './crypto.js';
+
+
+
 
 //connect the client to the server
 var socket = io();
 
 
-//when a new user joins, display a welcome message
-socket.on('newUser', function(user, roomID) {
-    $('div#joinForm').remove();
-    $('div#chatBox').append('<p class="important">New user ' + user + ' has joined room ' + roomID + '</p>');
+export default class App {
 
-});
+  constructor() {
+    this.init();
+  }
 
-//when a new message is recieved from the server, display it with
-//the username that broacasted it
-socket.on('recieveMessage', function(msg, user) {
-    $('div#chatBox').append('<p class="important">' + user + ': </p><p>' + msg + '</p>');
-    $('div#chatBox').animate({
-        scrollTop: $('div#chatBox').height()
-    }, 1000);
-});
 
-//when a new user joins, update the list of users currently in the room
-socket.on('updateUsers', function(list) {
-    $('div#userList').empty();
-    $('div#userList').append('<p>Users:</p>')
-    $.each(list, function(key, value) {
-        $('div#userList').append('<p class="important">' + value + '</p>');
+  init(){
+
+    //when a new user joins, display a welcome message
+    socket.on('newUser', function(user, roomID) {
+        $('div#joinForm').remove();
+        $('div#chatBox').append('<p class="important">New user ' + user + ' has joined room ' + roomID + '</p>');
+
     });
-});
 
-socket.on('userDisconnected', function(username) {
-    $('div#chatBox').append('<p class="important">User ' + username + ' has disconnected </p>');
-});
+    //when a new message is recieved from the server, display it with
+    //the username that broacasted it
+    socket.on('recieveMessage', function(msg, user) {
+        $('div#chatBox').append('<p class="important">' + user + ': </p><p>' + msg + '</p>');
+        $('div#chatBox').animate({
+            scrollTop: $('div#chatBox').height()
+        }, 1000);
+    });
 
-//submit the form to join a room
-$('button#join').click(function() {
-    $('div#joinModal').hide();
-    var username = $('input#username').val();
-    var roomID = $('input#roomID').val();
-    socket.emit('joinRoom', username, roomID);
+    //when a new user joins, update the list of users currently in the room
+    socket.on('updateUsers', function(list) {
+        $('div#userList').empty();
+        $('div#userList').append('<p>Users:</p>')
+        $.each(list, function(key, value) {
+            $('div#userList').append('<p class="important">' + value + '</p>');
+        });
+    });
 
-})
+    socket.on('userDisconnected', function(username) {
+        $('div#chatBox').append('<p class="important">User ' + username + ' has disconnected </p>');
+    });
 
-//submit a message
-$("input#textBar").keyup(function(event){
-        if(event.keyCode == 13){
-            $("button#sendMessage").click();
-        }
-});
-$('button#sendMessage').click(function() {
-    var msg = $('input#textBar').val();
-    //clear the text box
-    $('input#textBar').val('');
-    socket.emit('sendMessage', msg);
-});
+    //submit the form to join a room
+    $('button#join').click(function() {
+        $('div#joinModal').hide();
+        var username = $('input#username').val();
+        var roomID = $('input#roomID').val();
+        socket.emit('joinRoom', username, roomID);
+
+    })
+
+    //submit a message
+    $("input#textBar").keyup(function(event){
+            if(event.keyCode == 13){
+                $("button#sendMessage").click();
+            }
+    });
+    $('button#sendMessage').click(function() {
+        var msg = $('input#textBar').val();
+        //clear the text box
+        $('input#textBar').val('');
+        socket.emit('sendMessage', msg);
+    });
+  }
+}
--- public/scripts/entry.js
+++ public/scripts/entry.js
@@ -1,1 +1,5 @@
-import CryptoSession from './crypto.js';
+import app from 'client.js';
+
+$(() => {
+  let app = new App();
+});
